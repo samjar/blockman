e4ba20f124e38723ee58d8bf2dda9a0da2c3f323
samjar <ayillon@gmail.com>
2016-08-09 18:53:15 -0400

    put everything in the blockman folder

---
+++ blockman/color.py
@@ -0,0 +1,9 @@
+import pygame
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+PINK = (255, 0, 255)
+YELLOW = (255, 255, 0)
---
+++ blockman/levels.py
@@ -0,0 +1,36 @@
+import pygame
+
+level1 = [
+"PPPPPPPPPPPPPPPPPPPP",
+"P P                P",
+"P PE               P",
+"P PPPPPPPP  PPP  P P",
+"P   PP             P",
+"P   P              P",
+"P   P           PPPP",
+"P            PPPPPPP",
+"P         PPPPPPPPPP",
+"P      PPPPPPPPPPPPP",
+"PPPPPPPPPPPPPPPPPPPP",]
+
+level2 = [
+	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+	"P P                          P",
+	"P P    PPP          P      P P",
+	"P P P       P    P     P   P P",
+	"P P              P     P   P P",
+	"P P      D                 P P",
+	"P P    PPPPPP             PP P",
+	"P P   PPPPPPP  PPP    DP   P P",
+	"P P   PPPPPPP  PPP PPPPPPPPP P",
+	"P P PPPPPPPPP  PPP P         P",
+	"P P      PPPP  PPP P         P",
+	"P P      PPPP  PPP P         P",
+	"P P         P  PPP P         P",
+	"P P         P      PP    PPPDP",
+	"P PJPPPP    P  PPPPPPP  PPPPPP",
+	"P PPPPP    PP  PPP       P   P",
+	"P           P  PPP    PPPP   P",
+	"P       P   P  PPP  D        P",
+	"P     PPPD     PPP         E P",
+	"PPPPPPPPPPPPPPJPPPPPPPPPPPPPPP"]
