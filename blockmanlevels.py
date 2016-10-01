1f35346bb2dc412c75811ae513d9c07e8069f9dd
samjar <ayillon@gmail.com>
2016-10-01 12:48:25 -0400

    get dat hot potato mang

---
+++ blockmanlevels.py
@@ -0,0 +1,68 @@
+class BlockManLevels():
+    current_level = 0
+    levels = [
+
+    [
+	"PPPPPPPPPPPPPPPPPPPP",
+	"P P                P",
+	"P PC               P",
+	"P PPPPPPPP  PPP  P P",
+	"P   PP             P",
+	"P   P              P",
+	"P   P           PPPP",
+	"P            PPPPPPP",
+	"P         PPPPPPPPPP",
+	"P      PPPPPPPPPPPPP",
+	"PPPPPPPPPPPPPPPPPPPP"],
+
+	[
+	"PPPPPPPPPPPPPPPPPPPP",
+	"P PE   PPPPPPPPPPPPP",
+	"P PP   PPPPPPPPPPPPP",
+	"P                  P",
+	"P   P              P",
+	"P            PP    P",
+	"P      PPPP        P",
+	"P      PPPP        P",
+	"P      PPPP     P  P",
+	"P                  P",
+	"PPPPPPPPPPPPPPPPPPPP"],
+
+	[
+    "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+    "P                            P",
+    "p             PPPPPPP        P",
+    "p          P                 p",
+    "p                            p",
+    "p           D                p",
+    "PPPPP   PPPPPPPPPPPPP        p",
+    "p                            p",
+    "p                          E p",
+    "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"],
+
+	[
+    "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+	"P P                          P",
+	"P P    PPP          P      P P",
+	"P P P       P    P     P   P P",
+	"P P              P     P   P P",
+	"P PP     D                 P P",
+	"P P    PPPPPP             PP P",
+	"P P   PPPPPPP  PPP    DP   P P",
+	"P P   PPPPPPP  PPP PPPPPPPPP P",
+	"P P PPPPPPPPP  PPP P         P",
+	"P P      PPPP  PPP P         P",
+	"P PP     PPPP  PPP P         P",
+	"P  P        P  PPP P         P",
+	"P   P       P      PP    PPPDP",
+	"P    PPP    P  PPPPPPP  PPPPPP",
+	"P    PP    PP  PPP       P   P",
+	"P           P  PPP    PPPP   P",
+	"P       P   P  PPP  D        P",
+	"P   C PPPD     PPP         C P",
+	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"],
+  
+    ]
+
+if __name__ == "__main__":
+    print BlockManLevels.level_list
