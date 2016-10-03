90fa0c1a8b6dd9d14d0d32ebf129ed155c9e57f9
samjar <ayillon@gmail.com>
2016-10-02 20:55:10 -0400

    blockman

--- blockman/blockman.py
+++ blockman/blockman.py
@@ -30,7 +30,7 @@ soundItem = mixer.Sound("item.wav")
 soundJumpBlock = mixer.Sound("jumpblock.wav")
 mixer.music.load("mathgrant_Space_Blocks.mp3")
 
-
+blockLevels = BlockManLevels()
 
 """ starts main function """
 def main():
@@ -68,8 +68,8 @@ def main():
 	# - defines x, y
 	x = y = 0
 
-	current_level = BlockManLevels.current_level
-	level = BlockManLevels.levels[current_level]
+	current_level = blockLevels.current_level
+	level = blockLevels.levels[current_level]
 
 	def build_level(x, y):
 
@@ -77,8 +77,8 @@ def main():
 
 		entities.add(player)
 
-		current_level = BlockManLevels.current_level
-		level = BlockManLevels.levels[current_level]
+		current_level = blockLevels.current_level
+		level = blockLevels.levels[current_level]
 
 		""" build the level """
 		# - checks each row and column
@@ -210,107 +210,188 @@ class Entity(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 
-""" create the Player class """
 class Player(Entity):
-	def __init__(self, x, y):
-		Entity.__init__(self)
-		self.speed_x = 0
-		self.speed_y = 0
-		# - player starts out not on the ground
-		self.onGround = False
- 		self.image = Surface((32, 32))
-		# - converts image to same pixel format as gameDisplay
-		self.image.convert()
-		self.image.fill(RED)
-		self.rect = Rect(x, y, 32, 32)
 
-		self.endStage = False
+    def __init__(self, x, y):
+        Entity.__init__(self)
+        self.speed_x = 0
+        self.speed_y = 0
+        # - player starts out not on the ground
+        self.onGround = False
+        self.image = Surface((32, 32))
+        # - converts image to same pixel format as gameDisplay
+        self.image.convert()
+        self.image.fill(RED)
+        self.rect = Rect(x, y, 32, 32)
+        self.endStage = False
 
-	def update(self, up, down, left, right, platforms):
-		if up:
-			# - only jump if on the ground
-			if self.onGround == True:
-				mixer.Sound.play(soundJump)
-				jump_func(self, 7)
-		if down:
-			# - pressing down doesn't do anything yet
-			pass
-		if left:
-			self.speed_x = -5
-		if right:
-			self.speed_x = 5
-		if not self.onGround:
-			# - if player is in air, add gravity
-			self.speed_y += 0.3
-			# - set the max falling speed
-			if self.speed_y > 30:
-				self.speed_y = 0
-		if not(left or right):
-			self.speed_x = 0
-		# - increase in x direction
-		self.rect.left += self.speed_x
-		# - do x-axis collisions
-		self.collide(self.speed_x, 0, platforms)
-		# - increase in y direction
-		self.rect.top += self.speed_y
-		# - assuming we're in the air
-		self.onGround = False
-		# - do  y-axis collisions
-		self.collide(0, self.speed_y, platforms)
+    # TODO: create code to easier change gravity, player movement
+    # and also easier to implement new 'physics'.
+    def update(self, up, down, left, right, platforms):
+        if up:
+            # - only jump if on the ground
+            print self.onGround
+            if self.onGround:
+                mixer.Sound.play(soundJump)
+                self.jump_func(7, blockLevels.gravityDirection)
+        if down:
+            # - pressing down doesn't do anything yet
+            pass
+        if left:
+            self.move_left(blockLevels.gravityDirection)
+        if right:
+            self.move_right(blockLevels.gravityDirection)
+        if not self.onGround:
+            # function defined at line 292
+            self.apply_gravity(
+                blockLevels.gravity, blockLevels.gravityDirection)
+            # # - if player is in air, add gravity
+        if not(left or right):
+            self.stop_moving(blockLevels.gravityDirection)
+        # - increase in x direction
+        self.rect.left += self.speed_x
+        # - do x-axis
+        self.collide(self.speed_x, 0, platforms)
+        # - increase in y direction
+        self.rect.top += self.speed_y
+        # - assuming we're in the air
+        self.onGround = False
+        # - do  y-axis collisions
+        self.collide(0, self.speed_y, platforms)
 
-	""" the collision function """
-	def collide(self, speed_x, speed_y, platforms):
-		for p in platforms:
-			# - check every collision between player and platforms
-			if sprite.collide_rect(self, p):
-				# - I don't really understand isistance. Yeaaaaah
-				if isinstance(p, ExitBlock):
-					event.post(event.Event(QUIT))
-				elif isinstance(p, ClearStageBlock):				
-					self.endStage = True
-					BlockManLevels.current_level += 1
-					print BlockManLevels.current_level
-					self.rect.left = 32
-					self.rect.top = 32
-				elif isinstance(p, DeathBlock):
-					mixer.Sound.play(soundHurt)
-					time.delay(300)
-					self.rect.left = 32
-					self.rect.top = 32
-				elif isinstance(p, JumpBlock):
-					if speed_y > 0:
-						# - calls the jump function (doesn't work properly atm)
-						# - if you walk on it, it runs twice.
-						print("weeeeee")
-						mixer.Sound.play(soundJumpBlock)
-						jump_func(self, 15)
-					else:
-						pass
-				elif isinstance(p, FakeBlock):
-					pass
-					
 
-				# re-locates player to the outside of platform x, y 
-				# coords if player passes its boundaries
-				elif speed_x > 0:
-					self.rect.right = p.rect.left
-				elif speed_x < 0:
-					self.rect.left = p.rect.right
-				elif speed_y > 0:
-					self.rect.bottom = p.rect.top
-					self.onGround = True
-					self.speed_y = 0
-				elif speed_y < 0:
-					self.rect.top = p.rect.bottom
-					# - add the code in the comment below to disable "ceiling gliding"
-					# - thus making the game much harder.
-					#self.speed_y = 0
+    """ the collision function """
+    def collide(self, speed_x, speed_y, platforms):
+        for p in platforms:
+            # - check every collision between player and platforms
+            if sprite.collide_rect(self, p):
+                # - I don't really understand isistance. Yeaaaaah
+                if isinstance(p, ExitBlock):
+                    event.post(event.Event(QUIT))
+                elif isinstance(p, ClearStageBlock):
+                    self.endStage = True
+                    blockLevels.current_level += 1
+                    print blockLevels.current_level
+                    self.rect.left = 32
+                    self.rect.top = 32
+                elif isinstance(p, DeathBlock):
+                    mixer.Sound.play(soundHurt)
+                    time.delay(300)
+                    self.rect.left = 32
+                    self.rect.top = 32
+                elif isinstance(p, JumpBlock):
+                    if speed_y > 0:
+                        # - calls the jump function (doesn't work properly atm)
+                        # - if you walk on it, it runs twice.
+                        print("weeeeee")
+                        mixer.Sound.play(soundJumpBlock)
+                        self.jump_func(15, blockLevels.gravityDirection)
+                    else:
+                        pass
+                elif isinstance(p, FakeBlock):
+                    pass
 
-def jump_func(self, jump_height):
-	#sets the jump height to whatever was passed into the argument
-	self.jump_height = jump_height
-	self.speed_y -= jump_height
+                # re-locates player to the outside of platform x, y
+                # coords if player passes its boundaries
+                # TODO: add onGround() to elif statements
+                elif speed_x > 0:
+                    self.rect.right = p.rect.left
+                    # self.onGround = self.isOnGround(
+                    #                 p, blockLevels.gravityDirection)
+                elif speed_x < 0:
+                    self.rect.left = p.rect.right
+                    # self.onGround = self.isOnGround(
+                    #                 p, blockLevels.gravityDirection)
+                elif speed_y > 0:
+                    self.rect.bottom = p.rect.top
+                    # self.onGround = self.isOnGround(
+                    #                 p, blockLevels.gravityDirection)
+                    self.speed_y = 0
+                elif speed_y < 0:
+                    self.rect.top = p.rect.bottom
+                    # self.onGround = self.isOnGround(
+                    #                 p, blockLevels.gravityDirection)
+                self.onGround = self.isOnGround(p, blockLevels.gravityDirection)
+                    # - add the code in the comment below to disable "ceiling gliding"
+                    # - thus making the game much harder.
+                    # self.speed_y = 0
 
+    # First checks gravity direction, then applies gravity in that direction.
+    def apply_gravity(self, gravity, direction):
+        if direction == 'down':
+            self.speed_y += gravity
+            if self.speed_y > 30:
+                self.speed_y = 0
+        elif direction == 'up':
+            self.speed_y -= gravity
+            if self.speed_y < -30:
+                self.speed_y = 0
+        elif direction == 'left':
+            self.speed_x -= gravity
+            if self.speed_x < -30:
+                self.speed_x = 0
+            # self.speed_y = 0
+        elif direction == 'right':
+            self.speed_x += gravity
+            if self.speed_x > 30:
+                self.speed_x = 0
+            # self.speed_y = 0
+
+    # Gets called when there's no left/right input
+    def stop_moving(self, direction):
+        if direction == 'down' or direction == 'up':
+            self.speed_x = 0
+        else:
+            self.speed_y = 0
+
+    # Gets called at left input, acts differently depending on gravity direction
+    def move_left(self, direction):
+        if direction == 'down' or direction == 'up':
+            self.speed_x = -5
+        elif direction == 'left':
+            self.speed_y = -5
+        elif direction == 'right':
+            self.speed_y = 5
+
+    def move_right(self, direction):
+        if direction == 'down' or direction == 'up':
+            self.speed_x = 5
+        elif direction == 'left':
+            self.speed_y = 5
+        elif direction == 'right':
+            self.speed_y = -5
+
+    def jump_func(self, jump_height, direction):
+        # sets the jump height to whatever was passed into the argument
+        self.jump_height = jump_height
+        if direction == 'down':
+            self.speed_y -= jump_height
+        elif direction == 'up':
+            self.speed_y += jump_height
+        elif direction == 'left':
+            self.speed_x += jump_height
+        elif direction == 'right':
+            self.speed_x -= jump_height
+
+    # Check if player is on ground. Ground changes depending on
+    # current gravity direction.
+    def isOnGround(self, p, direction):
+        # If gravity direction is 'down', the function will check
+        # collision of players bottom border and platforms top border
+        # and return True if collision is detected
+        if direction == 'down':
+            if self.rect.bottom == p.rect.top:
+                return True
+        elif direction == 'up':
+            if self.rect.top == p.rect.bottom:
+                return True
+        elif direction == 'left':
+            if self.rect.left == p.rect.right:
+                return True
+        elif direction == 'right':
+            if self.rect.right == p.rect.left:
+                return True
+        return False
 
 """ creates the platform class, inherit the Entity class """
 class Platform(Entity):
--- blockman/blockmanlevels.py
+++ blockman/blockmanlevels.py
@@ -1,68 +1,90 @@
 class BlockManLevels():
-    current_level = 0
-    levels = [
 
-    [
-	"PPPPPPPPPPPPPPPPPPPP",
-	"P P                P",
-	"P PC               P",
-	"P PPPPPPPP  PPP  P P",
-	"P   PP             P",
-	"P   P              P",
-	"P   P           PPPP",
-	"P            PPPPPPP",
-	"P         PPPPPPPPPP",
-	"P      PPPPPPPPPPPPP",
-	"PPPPPPPPPPPPPPPPPPPP"],
+    def __init__(self):
+        self.current_level = 0
+        self.gravity = 0.3
+        self.gravityDirection = 'down'
 
-	[
-	"PPPPPPPPPPPPPPPPPPPP",
-	"P PC   PPPPPPPPPPPPP",
-	"P PP   PPPPPPPPPPPPP",
-	"P                  P",
-	"P   P              P",
-	"P            PP    P",
-	"P      PPPP        P",
-	"P      PPPP        P",
-	"P      PPPP     P  P",
-	"P                  P",
-	"PPPPPPPPPPPPPPPPPPPP"],
+    levels = [[
+        "PPPPPPPPPPPPPPPPPPPP",
+        "P P                P",
+        "P PC               P",
+        "P PPPPPPPP  PPP  P P",
+        "P   PP             P",
+        "P   P              P",
+        "P   P           PPPP",
+        "P            PPPPPPP",
+        "P         PPPPPPPPPP",
+        "P      PPPPPPPPPPPPP",
+        "PPPPPPPPPPPPPPPPPPPP"],
 
-	[
-    "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-    "P                            P",
-    "P             PPPPPPP        PHHHHHHHHHHHHHHHHHHHHHHHHPPPPPP",
-    "P          P                 F                             P",
-    "P                        HHHHPHHHHHHHHHHHHHHHHHHHHHHHHP    P",
-    "P           D                P                        P   CP",
-    "PPPPP   PPPPPPPPPPPPP        P                        PPPPPP",
-    "P                            P",
-    "P                          C P",
-    "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"],
+        [
+        "PPPPPPPPPPPPPPPPPPPP",
+        "P  C   PPPPPPPPPPPPP",
+        "P PP   PPPPPPPPPPPPP",
+        "P                  P",
+        "P   P              P",
+        "P            PP    P",
+        "P      PPPP        P",
+        "P      PPPP        P",
+        "P      PPPP     P  P",
+        "P                  P",
+        "PPPPPPPPPPPPPPPPPPPP"],
 
-	[
-    "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-	"P P                          P",
-	"P P    PPP          P      P P",
-	"P P P       P    P     P   P P",
-	"P P              P     P   P P",
-	"P PP     D                 P P",
-	"P P    PPPPPP             PP P",
-	"P P   PPPPPPP  PPP    DP   P P",
-	"P P   PPPPPPP  PPP PPPPPPPPP P",
-	"P P PPPPPPPPP  PPP P         P",
-	"P P      PPPP  PPP P         P",
-	"P PP     PPPP  PPP P         P",
-	"P  P        P  PPP P         P",
-	"P   P       P      PP    PPPDP",
-	"P    PPP    P  PPPPPPP  PPPPPP",
-	"P    PP    PP  PPP       P   P",
-	"P           P  PPP    PPPP   P",
-	"P       P   P  PPP  D        P",
-	"P     PPPD     PPP         C P",
-	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"],
-  
+        [
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+        "P                            P",
+        "P             PPPPPPP        PHHHHHHHHHHHHHHHHHHHHHHHHPPPPPP",
+        "P          P                 F                             P",
+        "P                        HHHHPHHHHHHHHHHHHHHHHHHHHHHHHP    P",
+        "P           D                P                        P   CP",
+        "PPPPP   PPPPPPPPPPPPP        P                        PPPPPP",
+        "P                            P",
+        "P                          C P",
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"],
+
+        [
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+        "P                            P",
+        "P             PPPPPPP        P",
+        "P          P                 p",
+        "P                            P",
+        "P           D                P",
+        "PPPPP   PPPPPPPPPPPPP        P",
+        "P                            P",
+        "P                          C P",
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"],
+
+        [
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+        "P P                          P",
+        "P P    PPP          P      P P",
+        "P P P       P    P     P   P P",
+        "P P              P     P   P P",
+        "P PP     D                 P P",
+        "P P    PPPPPP             PP P",
+        "P P   PPPPPPP  PPP    DP   P P",
+        "P P   PPPPPPP  PPP PPPPPPPPP P",
+        "P P PPPPPPPPP  PPP P         P",
+        "P P      PPPP  PPP P         P",
+        "P PP     PPPP  PPP P         P",
+        "P  P        P  PPP P         P",
+        "P   P       P      PP    PPPDP",
+        "P    PPP    P  PPPPPPP  PPPPPP",
+        "P    PP    PP  PPP       P   P",
+        "P           P  PPP    PPPP   P",
+        "P       P   P  PPP  D        P",
+        "P     PPPD     PPP         C P",
+        "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"],
+
     ]
+
+    def levelTwo(self):
+        self.gravity = 0.2
+        self.gravityDirection = 'left'
+
+    def level50(self):
+        self.gravityDirection = 'right'
 
 
 if __name__ == "__main__":
